6e90c7ca721bba056b92f6f06b532a5bdc99f216
dindinet <2939526+dindinet@users.noreply.github.com>
2023-05-26 15:44:48 +0100

    return to thanks

--- netlify/functions/form-handler.js
+++ netlify/functions/form-handler.js
@@ -1,48 +1,19 @@
 const fetch = require('node-fetch');
 
 exports.handler = async function(event) {
-  if (event.httpMethod !== 'GET') {
-    return {
-      statusCode: 405,
-      body: 'Method Not Allowed'
-    };
-  }
+  //if (event.httpMethod !== 'GET') {
+ //   return {
+ //     statusCode: 405,
+ //     body: 'Method Not Allowed'
+ //   };
+ // }
 
-  const formData = JSON.parse(event.body);
-  console.log('formData');
+  //const formData = JSON.parse(event.body);
+  //console.log('formData');
   return {
     statusCode: 302,
     headers: {
         "Location": "/thanks.html?" + new URLSearchParams(event.queryStringParameters),
     },
-};
-  /*
-  try {
-    const response = await fetch('https://script.google.com/macros/s/AKfycbxCg6EMHb1lk5HhFr0VoGIbUw8mylhq9OSpyFGmnuiMbtkAGKgm0XE19puQWebqwJPW/exec', {
-      method: 'POST',
-      body: JSON.stringify(formData),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    });
-   
-    if (!response {
-      throw new Error('Failed to submit form to remote API');
-    }
-    if (response){
-      console.log('response.data')
-      console.log(JSON.stringify(response.data))
-    }
-    return {
-      statusCode: 200,
-      headers: {"Content-Type": "application/json"},
-      body: JSON.stringify(response.data)
-    } 
-  } catch(err){
-    body: JSON.stringify({
-      status: 'FAIL',
-      message: err.message
-    })
-  }
-  */
-  }
+  };
+}
