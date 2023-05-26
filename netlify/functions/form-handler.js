727034e556b0fb5db2f4ff1cbdffad7c99ab503e
dindinet <2939526+dindinet@users.noreply.github.com>
2023-05-26 19:19:12 +0100

    send an email with mailersend

--- netlify/functions/form-handler.js
+++ netlify/functions/form-handler.js
@@ -34,6 +34,27 @@ if (event.httpMethod == 'POST') {
 
    console.log(JSON.stringify(formdata))
 
+// Send email using mailersend API
+emailmessage = JSON.stringify({
+  "from": {"email": "info@thaiyom.com", "name": "Web Form"},
+      "to": [{"email": "elena@dindi.net"},{"email":"dklongley@gmail.com"}],
+  "subject": "Health Questionaire",
+  "text": `name: {$formdata.fname} {$formdata.lname}\n email: {$formdata.email}\n phone: {$formdata.phone}\n`,
+  "html": "`name: {$formdata.fname} {$formdata.lname}<br> email: {$formdata.email}<br> phone: {$formdata.phone}<br>`"
+})
+
+fetch('https://api.mailersend.com/v1/email', {
+method: 'POST',
+body: emailmessage,
+headers: {
+'Content-type': 'text/html; charset=UTF-8',
+'Authorization': 'Bearer mlsn.be2cae5b751cea33edd0b813abacccb31ee9f0269b1425b05333c5fba6c6566a',
+},
+})
+.then((response) => response.json())
+.then((json) => console.log(json));
+   
+
   return {
     statusCode: 302,
     headers: {
